0506f68922c4dd07c1c94b536825466a9831a34d
Mahe Karim <mahekarim@gmail.com>
2022-06-30 04:32:07 +0600

    First C# Deploy :boom:

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace myProject {
+    class Employee {
+      static void Main(string[] args) {
+        // Console.WriteLine("Help Poor!");
+        string? first_name, last_name;
+
+        Console.Write("Type your First Name : ");
+        first_name = Console.ReadLine();
+
+        Console.Write("Type Your Last Name : ");
+        last_name = Console.ReadLine();
+
+        Console.Write("Here Your First Name : " + first_name + " And Your Last Name : " + last_name);
+        // Console.ReadKey();
+
+        } // Main Method End
+
+    } // Class End
+}
